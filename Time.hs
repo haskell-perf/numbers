c276a635a3fc14f0f7d143d46ffe2bb9963b7409
Chris Done <chrisdone@gmail.com>
2017-03-27 10:50:58 +0100

    Fix naming

--- Time.hs
+++ Time.hs
@@ -3,8 +3,9 @@ module Main (main) where
 import Control.Monad
 import Criterion.Main
 import Criterion.Types
-import System.Directory
+import Data.Char
 import Data.Scientific
+import System.Directory
 
 main :: IO ()
 main = do
@@ -16,38 +17,38 @@ main = do
     [ bgroup
         "Addition"
         (concat
-           [ [ bench ("Int:" ++ show i) (whnf add'Int (-i))
+           [ [ bench ("Int:" ++ scale i) (whnf add'Int (-i))
              | i <- [1, 10, 100, 1000, 10000, 100000, 1000000]
              ]
-           , [ bench ("Integer:" ++ show i) (whnf add'Integer (-i))
+           , [ bench ("Integer:" ++ scale i) (whnf add'Integer (-i))
              | i <- [1, 10, 100, 1000, 10000, 100000, 1000000]
              ]
-           , [ bench ("Rational:" ++ show i) (whnf add'Rational (-i))
+           , [ bench ("Rational:" ++ scale i) (whnf add'Rational (-i))
              | i <- [1, 10, 100, 1000, 10000, 100000, 1000000]
              ]
-           , [ bench ("Double:" ++ show i) (whnf add'Double (-i))
+           , [ bench ("Double:" ++ scale i) (whnf add'Double (-i))
              | i <- [1, 10, 100, 1000, 10000, 100000, 1000000]
              ]
-           , [ bench ("Scientific:" ++ show i) (whnf add'Scientific (-i))
+           , [ bench ("Scientific:" ++ scale i) (whnf add'Scientific (-i))
              | i <- [1, 10, 100, 1000, 10000, 100000, 1000000]
              ]
            ])
     , bgroup
         "Subtraction"
         (concat
-           [ [ bench ("Int:" ++ show i) (whnf subtract'Int i)
+           [ [ bench ("Int:" ++ scale i) (whnf subtract'Int i)
              | i <- [1, 10, 100, 1000, 10000, 100000, 1000000]
              ]
-           , [ bench ("Integer:" ++ show i) (whnf subtract'Integer i)
+           , [ bench ("Integer:" ++ scale i) (whnf subtract'Integer i)
              | i <- [1, 10, 100, 1000, 10000, 100000, 1000000]
              ]
-           , [ bench ("Rational:" ++ show i) (whnf subtract'Rational i)
+           , [ bench ("Rational:" ++ scale i) (whnf subtract'Rational i)
              | i <- [1, 10, 100, 1000, 10000, 100000, 1000000]
              ]
-           , [ bench ("Double:" ++ show i) (whnf subtract'Double i)
+           , [ bench ("Double:" ++ scale i) (whnf subtract'Double i)
              | i <- [1, 10, 100, 1000, 10000, 100000, 1000000]
              ]
-           , [ bench ("Scientific:" ++ show i) (whnf subtract'Scientific i)
+           , [ bench ("Scientific:" ++ scale i) (whnf subtract'Scientific i)
              | i <- [1, 10, 100, 1000, 10000, 100000, 1000000]
              ]
            ])
@@ -83,3 +84,7 @@ main = do
     add'Scientific :: Scientific -> ()
     add'Scientific 0 = ()
     add'Scientific a = add'Scientific (a + 1)
+
+-- | Show without any % 1 or .0 extensions.
+scale :: Show a => a -> String
+scale = takeWhile isDigit . show
